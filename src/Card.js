2114f53972ebfbfcfd9a4c9d678c103dc10e8199
rohithrajesh1 <48838176+rohithrajesh1@users.noreply.github.com>
2020-04-21 00:10:48 +0530

    added js and css files

---
+++ src/Card.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+const card=({id,email,name})=>{
+	return(
+        //<h1>Robofriends</h1>
+		<div className='tc bg-light-green dib br3 pa2 ma2 grow shadow-5'>
+			<img alt='robots' src={`https://robohash.org/${id}?200X200`} />
+            <div>
+                <h2>{name}</h2>
+                <p>{email}</p>
+            </div>
+		</div>
+
+	);
+}
+
+export default card;
